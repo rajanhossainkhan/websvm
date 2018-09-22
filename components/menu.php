9eab8a01949b6e668df39d3415e2de016556c2bd
Rajan Hossain <rajanhossainkhan@gmail.com>
2018-09-23 01:55:18 +0800

    Signup added in the menu

--- components/menu.php
+++ components/menu.php
@@ -2,5 +2,6 @@
 	<a style="text-decoration:none; color:black;" href="index.php">Home</a> |
 	<a style="text-decoration:none; color:black;" href="text_process.php">Keyword Extractor</a> |
 	<a style="text-decoration:none; color:black;" href="sparse_matrix.php">Sparse Matrix</a> |
-	<a style="text-decoration:none; color:black;" href="process.php">Prediction</a>
+	<a style="text-decoration:none; color:black;" href="process.php">Prediction</a> |
+	<a style="text-decoration:none; color:black;" href="signup.php">Signup</a> |
 </div>
