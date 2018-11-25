24a2347055606c3d14d3c940a9909c457f9634b9
Rajan Hossain <rajanhossainkhan@gmail.com>
2018-11-25 17:07:09 +0800

    Addition of easy.py in progress.

--- DB/websvm.sql
+++ DB/websvm.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50635
  File Encoding         : 65001
 
- Date: 21/10/2018 01:23:15
+ Date: 23/11/2018 00:59:21
 */
 
 SET NAMES utf8mb4;
@@ -26,13 +26,13 @@ CREATE TABLE `UserFiles` (
   `FileNameGiven` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `FileName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `FileIdentifier` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `FileType` enum('train','test','model') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `FileType` enum('train','test','model','result') COLLATE utf8_unicode_ci DEFAULT NULL,
   `FilePath` text COLLATE utf8_unicode_ci NOT NULL,
   `UserId` int(11) DEFAULT NULL,
   `UpdateDate` datetime DEFAULT NULL,
   `UpdateBy` int(11) DEFAULT NULL,
   PRIMARY KEY (`FileId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;;
 
 -- ----------------------------
 -- Records of UserFiles
@@ -41,28 +41,18 @@ BEGIN;
 INSERT INTO `UserFiles` VALUES (1, '', 'train_master.txt', '', '', '../Uploads/TrainingFiles/1/1540053665.14_ train_master.txt', 1, '2018-10-20 18:41:05', 1);
 INSERT INTO `UserFiles` VALUES (2, '', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1540053723.3_ train_master.txt', 1, '2018-10-20 18:42:03', 1);
 INSERT INTO `UserFiles` VALUES (3, '', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1540054599.39_ train_master.txt', 1, '2018-10-20 18:56:39', 1);
-COMMIT;
-
--- ----------------------------
--- Table structure for user_accounts
--- ----------------------------
-DROP TABLE IF EXISTS `user_accounts`;
-CREATE TABLE `user_accounts` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_fullname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email_address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `create_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `mod_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;;
-
--- ----------------------------
--- Records of user_accounts
--- ----------------------------
-BEGIN;
-INSERT INTO `user_accounts` VALUES (1, 'Rajan', 'rajan.hossain@yahoo.com', '123456', '2018-09-22 16:51:37', '2018-09-22 16:51:37');
-INSERT INTO `user_accounts` VALUES (2, 'Test', 'rajan.hossain@yahoo.com', '123456', '2018-09-22 17:05:06', '2018-09-22 17:05:06');
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `UserFiles` VALUES (4, 'Training File', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1540118505.69_train_master.txt', 1, '2018-10-21 12:41:45', 1);
+INSERT INTO `UserFiles` VALUES (5, '', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1540123680.46_train_master.txt', 1, '2018-10-21 14:08:00', 1);
+INSERT INTO `UserFiles` VALUES (6, '', '', '', 'model', '../LearningModels/1/1540123680.46_train_master.txt.model', 1, '2018-10-21 14:29:24', 1);
+INSERT INTO `UserFiles` VALUES (7, '', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1541509507.65_train_master.txt', 1, '2018-11-06 14:05:07', 1);
+INSERT INTO `UserFiles` VALUES (8, '', 'train_master.txt', '', 'model', '../LearningModels/1/1541509507.65_train_master.txt.model', 1, '2018-11-06 14:05:12', 1);
+INSERT INTO `UserFiles` VALUES (9, '', 'train_master.txt', '', 'model', '../LearningModels/1/1541509507.65_train_master.txt.model', 1, '2018-11-06 14:08:52', 1);
+INSERT INTO `UserFiles` VALUES (10, '', 'train_master.txt', '', 'model', '../LearningModels/1/1541509507.65_train_master.txt.model', 1, '2018-11-06 14:10:30', 1);
+INSERT INTO `UserFiles` VALUES (11, 'Training file', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1541866379.05_train_master.txt', 1, '2018-11-10 17:12:59', 1);
+INSERT INTO `UserFiles` VALUES (12, '', 'train_master.txt', '', 'model', '../LearningModels/1/1541866379.05_train_master.txt.model', 1, '2018-11-10 17:13:05', 1);
+INSERT INTO `UserFiles` VALUES (13, 'Training File Master', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1542457470.63_train_master.txt', 1, '2018-11-17 13:24:30', 1);
+INSERT INTO `UserFiles` VALUES (14, 'Master training file', 'train_master.txt', '', 'train', '../Uploads/TrainingFiles/1/1542457581.2_train_master.txt', 1, '2018-11-17 13:26:21', 1);
+INSERT INTO `UserFiles` VALUES (15, '', 'train_master.txt', '', 'model', '../LearningModels/1/1542457581.2_train_master.txt.model', 1, '2018-11-17 13:26:29', 1);
+INSERT INTO `UserFiles` VALUES (16, '', 'test_master.txt', '', 'test', '../TestDatasets/1//1542458658.61_test_master.txt', 1, '2018-11-17 13:44:18', 1);
+INSERT INTO `UserFiles` VALUES (17, '', 'test_master.txt', '', 'test', '../TestDatasets/1//1542461246.63_test_master.txt', 1, '2018-11-17 14:27:26', 1);
+INSERT INTO `UserFiles` VALUES (18, '', 'test_master.txt', '', 'test',
--- app/training.php
+++ app/training.php
@@ -48,6 +48,7 @@ if (isset($_POST["frmSubmit"])){
 	 * Check the directory before running the training command
 	 * Depending on the file size, trainign might take longer time
 	 */
+	
 
 	//Create a directory or check exisiting
 	//Create directory for the user if not yet created
@@ -56,6 +57,57 @@ if (isset($_POST["frmSubmit"])){
 		mkdir($file_path, 0777, true);
 	}
 	$fully_qualified_path = $file_path . "/" . $TargetFile . ".model";
+
+
+	/**
+	* Assume uploaded file is a right svm file
+	* Run train command against it.
+	*/
+	shell_exec("../libsvm/./svm-train " . $files_directory . $user_id_session . "/" . $TargetFile);
+	
+	//Based on the output from the shell,
+	//Determine if the process completed successfully
+
+	//Once the process is complete, make record of model file in db
+	$insert_array = array(
+		"FileNameGiven" => $trainingFileTitle,
+		"FileName" => $TargetFile,
+		"FileType" => "model",
+		"FilePath" => $fully_qualified_path,
+		"UserId" => $user_id_session,
+		"UpdateDate" => date("Y-m-d H:i:s"),
+		"UpdateBy" => $user_id_session
+	);
+	$con->insert("UserFiles", $insert_array);
+	
+		$msg = "Classification is successfull. A result file is generated
+	<a href='predict.php?TargetFile=".$TargetFile.".model' class='btn btn-primary'> Proceed to Result</a>";
+}
+
+if (isset($_POST['frmSubmitAuto'])){
+	echo "Working";
+	/**
+	 * Check the directory before running the training command
+	 * Depending on the file size, trainign might take longer time
+	 */
+
+	//Create a directory or check exisiting
+	//Create directory for the user if not yet created
+	$file_path = '../LearningModels/' . $_SESSION['user_id'];
+	if (!file_exists($file_path)) {
+		mkdir($file_path, 0777, true);
+	}
+	echo $fully_qualified_path = $file_path . "/" . $TargetFile . ".model";
+
+	//Easy.py command generates and output
+	//The filename has an scale.out added at the end of it.
+	$file_path_output = '../OutputFiles/' . $_SESSION['user_id'];
+	if (!file_exists($file_path_output)) {
+		mkdir($file_path_output, 0777, true);
+	}
+	echo $fully_qualified_path_output = $file_path_output . "/" . $TargetFile . "scale.out";
+	exit();
+
 
 	//Make record in database
 	//Insert a record in the db for uploaded file for the user
@@ -79,6 +131,21 @@ if (isset($_POST["frmSubmit"])){
 	} else {
 		$err = "Something went wrong. Training failed.";
 	}
+
+	//Make record of output file (SVM) in the db
+	//Easy.py, unlike default prediction model,
+	//performs cross validation and parameter tuning
+	//Output file would contain the combination of C and G.
+	$insert_array = array(
+		"FileNameGiven" => "",
+		"FileName" => $TargetFile . ".scale.out",
+		"FileType" => "output",
+		"FilePath" => $fully_qualified_path,
+		"UserId" => $user_id_session,
+		"UpdateDate" => date("Y-m-d H:i:s"),
+		"UpdateBy" => $user_id_session
+	);
+	$con->insert("UserFiles",$insert_array);
 }
 ?>
 
@@ -96,6 +163,7 @@ if (isset($_POST["frmSubmit"])){
 		    <br />
 		    <br />
 		    <input type="submit" value="Run Train Command" name="frmSubmit" class="btn btn-primary">
+		    <input type="submit" value="Run Automated Classification" name="frmSubmitAuto" class="btn btn-primary">
 		</form>
 	</div>
 </body>
